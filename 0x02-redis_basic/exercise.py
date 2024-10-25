b0c8a4b5a7484ec742ece947f2ea40eb7c592163
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-10-25 17:30:31 +0300

    Task 2.Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,7 @@ Cache class with Redis client
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -27,3 +27,50 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve data from Redis and optionally convert it using a callable.
+
+        Parameters:
+            key (str): The key to retrieve from Redis.
+            fn (Optional[Callable]): A callable to convert the data.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data or
+            None if the key does not exist.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve a string from Redis.
+
+        Parameters:
+            key (str): The key to retrieve from Redis.
+
+        Returns:
+            Optional[str]: The retrieved string or
+            None if the key does not exist.
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve an integer from Redis.
+
+        Parameters:
+            key (str): The key to retrieve from Redis.
+
+        Returns:
+            Optional[int]: The retrieved integer or
+            None if the key does not exist.
+        """
+        return self.get(key, fn=int)
