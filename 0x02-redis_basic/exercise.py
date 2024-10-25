943a9017ac37e92f8bfbd992157e15f40c64eba2
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-10-25 17:13:32 +0300

    0.Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Cache class with Redis client
+"""
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        """Initialize the Redis client and flush the database"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store the input data in Redis using a random key and return the key.
+
+        Parameters:
+            data (Union[str, bytes, int, float]): The data to store in Redis.
+
+        Returns:
+            str: The key under which the data is stored.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
